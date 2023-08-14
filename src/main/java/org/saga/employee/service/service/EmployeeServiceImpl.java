c6897be2bdda10567ab66d2d2267e622241e6417
Sagar Waghunde <sagar@Sagars-MacBook-Pro.local>
2023-08-14 12:40:35 +0800

    added circuit breaker for calling dept service

--- src/main/java/org/saga/employee/service/service/EmployeeServiceImpl.java
+++ src/main/java/org/saga/employee/service/service/EmployeeServiceImpl.java
@@ -14,45 +14,61 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
 //	private RestTemplate restTemplate;
-//	private WebClient webClient;
-	private APIClient apiClient;
-	
+	private WebClient webClient;
+//	private APIClient apiClient;
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
 	@Override
 	public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
 		employeeRepository.findByEmail(employeeDto.getEmail()).ifPresent(employee -> {
-			throw new EmailAlreadyExistsException(String.format("Employee with '%s' email address already exists", employee.getEmail()));
+			throw new EmailAlreadyExistsException(
+					String.format("Employee with '%s' email address already exists", employee.getEmail()));
 		});
 		Employee entity = EmployeeMapper.MAPPER.mapToEmployee(employeeDto);
-		Employee savedEntity = employeeRepository.save(entity); 	
+		Employee savedEntity = employeeRepository.save(entity);
 		return EmployeeMapper.MAPPER.mapToEmployeeDto(savedEntity);
 	}
 
+	@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
 	@Override
 	public APIResponseDto getEmployeeById(Long employeId) {
-		Employee employee = employeeRepository.findById(employeId).orElseThrow(
-			() -> new ResourceNotFoundException("Employee", "id", employeId.toString())
-		);
-		
+		Employee employee = employeeRepository.findById(employeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeId.toString()));
+
 //		ResponseEntity<DepartmentDto> response = restTemplate.getForEntity("http://localhost:8080/api/v1/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
 //		DepartmentDto departmentDto = response.getBody();
-//		DepartmentDto departmentDto = webClient.get()
-//			.uri("http://localhost:8080/api/v1/departments/" + employee.getDepartmentCode())
-//			.retrieve()
-//			.bodyToMono(DepartmentDto.class)
-//			.block(); // synchronous call
+		DepartmentDto departmentDto = webClient.get()
+				.uri("http://localhost:8080/api/v1/departments/" + employee.getDepartmentCode()).retrieve()
+				.bodyToMono(DepartmentDto.class).block(); // synchronous call
 
-		DepartmentDto departmentDto = apiClient.getDepartmentByCode(employee.getDepartmentCode());
+//		DepartmentDto departmentDto = apiClient.getDepartmentByCode(employee.getDepartmentCode());
 		EmployeeDto employeeDto = EmployeeMapper.MAPPER.mapToEmployeeDto(employee);
 		APIResponseDto apiResponseDto = new APIResponseDto(employeeDto, departmentDto);
 		return apiResponseDto;
+	}
+
+	public APIResponseDto getDefaultDepartment(Long employeId, Exception exception) {
+		Employee employee = employeeRepository.findById(employeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeId.toString()));
+
+		DepartmentDto departmentDto = new DepartmentDto();
+		departmentDto.setDepartmentCode("DF001");
+		departmentDto.setDepartmentName("Default Dept");
+		departmentDto.setDepartmentDescription("this is default department");
+
+		EmployeeDto employeeDto = EmployeeMapper.MAPPER.mapToEmployeeDto(employee);
+		APIResponseDto apiResponseDto = new APIResponseDto(employeeDto, departmentDto);
+		return apiResponseDto;
+
 	}
 
 }
